24c2dcfcc22d07347a289b52bce940b06d51eb00
Dmitry Kuchukov <kudim84@gmail.com>
2022-11-11 22:10:09 +0300

    feat: add files into images

--- birds.js
+++ birds.js
@@ -5,7 +5,7 @@ const birdsData = [
         name: 'Ворон',
         species: 'Corvus corax',
         description: 'Ворон – крупная птица. Длина тела достигает 70 сантиметров, размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра. В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет.',
-        image: 'https://live.staticflickr.com//65535//49298804222_474cfe8682.jpg',
+        image: 'https://raw.githubusercontent.com/dmitro84/data-birds/main/images/crow.webp',
         audio: 'https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3'
       },
       {
@@ -13,7 +13,7 @@ const birdsData = [
         name: 'Журавль',
         species: 'Grus grus',
         description: 'Звуки, издаваемые журавлем, похожи на звонкое «кур-лы – кур-лы». Журавли чаще всего поют дуэтом – одна птица начинает запев со слога «кур», а вторая подхватывает «лы». Если птица поёт одна, то она издает только звук «кур».',
-        image: 'https://live.staticflickr.com/65535/49221158846_b0b69a58f1.jpg',
+        image: 'https://raw.githubusercontent.com/dmitro84/data-birds/main/images/crain.png',
         audio: 'https://www.xeno-canto.org/sounds/uploaded/BLMSIUFTFU/XC512582-190604_1087_Grus_tok.mp3'
       },
       {
@@ -21,7 +21,7 @@ const birdsData = [
         name: 'Ласточка',
         species: 'Delichon urbicum',
         description: 'Для ласточек характерно негромкое щебетание. Песни ласточек не смолкают на протяжении всего лета. Исследователи различают у птиц до 6 щебечущих звуков: «вит», «ви-вит», «чивит», «чиривит» и т.п. Ласточки любят петь дуэтом.',
-        image: 'https://live.staticflickr.com//65535//48539007512_5029d2a9a0.jpg',
+        image: 'https://raw.githubusercontent.com/dmitro84/data-birds/main/images/martin.png',
         audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC489247-190724_09.10h_huiszwaluw_biesbosch_amaliahoeve_roep_100%2Bex_fouragerend_gezien_%20%282%29.mp3'
       },
       {
@@ -29,7 +29,7 @@ const birdsData = [
         name: 'Козодой',
         species: 'Caprimulgus europaeus',
         description: 'Козодой – неприметная птица, известная благодаря своему голосу. Песня козодоя звучит как монотонная трель похожая на тарахтение мотоцикла. Такое дребезжание слышно от заката до рассвета, его тональность, частота и громкость изменяются. ',
-        image: 'https://live.staticflickr.com/65535/48456345286_dbc8530027.jpg',
+        image: 'https://raw.githubusercontent.com/dmitro84/data-birds/main/images/nightjar.webp',
         audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC486956-190623_22.37h_nachtzwaluw_rechte%20heide_zang_ad%20_2ex_gezien_.mp3'
       },
       {
@@ -37,7 +37,7 @@ const birdsData = [
         name: 'Кукушка',
         species: 'Cuculus canorus',
         description: 'Кукушку назвали так из-за особенностей ее песен. Звонкое «ку-ку» не спутать ни с какой другой птицей. Кукушки не строят гнезда, их потомство выращивают другие виды пернатых, которым кукушки подбрасывают свои яйца.',
-        image: 'https://live.staticflickr.com/65535/48377838151_e15f430ec1.jpg',
+        image: 'https://raw.githubusercontent.com/dmitro84/data-birds/main/images/cuckoo.webp',
         audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC501461-190616_08.13h_koekoek_brabantse%20biesbosch%20jantjesplaat_roep_1%20ex_ad%20m_ter%20plaatse%20zingend_gezien_.mp3'
       },
       {
